74bcd1a0aeed212657446778c740ea0d1300bfef
Gilberto <gp_sj@hotmail.com>
2018-03-19 11:07:03 -0300

    Alteracao 19/03/18 2

--- lib/sedpool/guardian/sedpool_error_handler.ex
+++ lib/sedpool/guardian/sedpool_error_handler.ex
@@ -4,18 +4,6 @@ defmodule Sedpool.SedpoolErrorHandler do
   alias Sedpool.Account
   alias Sedpool.Account.User
 
-#  def auth_error(conn, {type, reason}, _opts) do
-#    body = Poison.encode!(%{message: to_string(type)})
-#    send_resp(conn, 401, body)
-#  end
-
-#  def auth_error(conn, {type, reason}, _opts) do
-#    body = to_string(type)
-#    conn
-#    |> put_resp_content_type("text/plain")
-#    |> send_resp(401, body)
-#  end
-
   def auth_error(conn, {type, reason}, _opts) do
 	conn
 	|> Phoenix.Controller.redirect(to: SedpoolWeb.Router.Helpers.session_path(conn, :new))
