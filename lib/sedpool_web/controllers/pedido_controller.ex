7320018e80216ac17f88761a4704cf98639ce966
Gilberto <gp_sj@hotmail.com>
2018-03-25 16:45:13 -0300

    Alteracao 25/03/18 3

--- lib/sedpool_web/controllers/pedido_controller.ex
+++ lib/sedpool_web/controllers/pedido_controller.ex
@@ -8,7 +8,7 @@ defmodule SedpoolWeb.PedidoController do
   alias Sedpool.Account.Cliente
   alias Sedpool.Repo
   import Ecto.Query  
-
+########################################################################################################
 #  def index(conn, _params) do
 #    pedidos = Account.list_pedidos()
 #    render(conn, "index.html", pedidos: pedidos)
@@ -18,7 +18,7 @@ defmodule SedpoolWeb.PedidoController do
             |> Repo.paginate(params)
     render(conn, "index.html", pedidos: page.entries, page: page)
   end
-
+#########################################################################################################
   def new(conn, _params) do
     changeset = Account.change_pedido(%Pedido{})
     cod_cliente = Repo.all(from(c in Cliente, select: {c.fantasia_cliente, c.cod_cliente}))
@@ -27,7 +27,6 @@ defmodule SedpoolWeb.PedidoController do
     condpagamento = Repo.all(from(c in Condpag, select: {c.descricao_condpagamento, c.codigo_condpagamento}))
     cod_vendedor = Repo.all(from(c in Vendedor, select: {c.cod_vendedor, c.cod_vendedor}))
 
-#    render(conn, "new.html", changeset: changeset)
      render(conn, "new.html", changeset: changeset, 
       condpagamento: condpagamento,
       cod_vendedor: cod_vendedor,
@@ -35,31 +34,55 @@ defmodule SedpoolWeb.PedidoController do
       tipo_cliente: tipo_cliente)
 
   end
-
+#########################################################################################################
+#  def create(conn, %{"pedido" => pedido_params}) do
+#    case Account.create_pedido(pedido_params) do
+#      {:ok, pedido} ->
+#        conn
+#        |> put_flash(:info, "Pedido created successfully.")
+#        |> redirect(to: pedido_path(conn, :show, pedido))
+#      {:error, %Ecto.Changeset{} = changeset} ->
+##        render(conn, "new.html", changeset: changeset)
+#     render(conn, "new.html", changeset: changeset,
+#	condpagamento: Repo.all(from(c in Condpag, select: {c.descricao_condpagamento, c.codigo_condpagamento})),
+#	cod_vendedor: Repo.all(from(c in Vendedor, select: {c.cod_vendedor, c.cod_vendedor})))
+#    end
+#  end
   def create(conn, %{"pedido" => pedido_params}) do
+    cod_cliente = Repo.all(from(c in Cliente, select: {c.fantasia_cliente, c.cod_cliente}))
+    tipo_cliente = Repo.all(from(c in Cliente, select: {c.tipo_cliente, c.tipo_cliente}))
+    condpagamento = Repo.all(from(c in Condpag, select: {c.descricao_condpagamento, c.codigo_condpagamento}))
+    cod_vendedor = Repo.all(from(c in Vendedor, select: {c.cod_vendedor, c.cod_vendedor}))
+  
     case Account.create_pedido(pedido_params) do
       {:ok, pedido} ->
         conn
         |> put_flash(:info, "Pedido created successfully.")
         |> redirect(to: pedido_path(conn, :show, pedido))
       {:error, %Ecto.Changeset{} = changeset} ->
-#        render(conn, "new.html", changeset: changeset)
-     render(conn, "new.html", changeset: changeset, condpagamento: Repo.all(from(c in Condpag, select: {c.descricao_condpagamento, c.codigo_condpagamento})), cod_vendedor: Repo.all(from(c in Vendedor, select: {c.cod_vendedor, c.cod_vendedor})))
+     render(conn, "new.html", changeset: changeset,
+      condpagamento: condpagamento,
+      cod_vendedor: cod_vendedor,
+      cod_cliente: cod_cliente,
+      tipo_cliente: tipo_cliente)
     end
   end
 
+################################################################################################################
   def show(conn, %{"id" => id}) do
     pedido = Account.get_pedido!(id)
     render(conn, "show.html", pedido: pedido)
   end
-
+################################################################################################################
   def edit(conn, %{"id" => id}) do
     pedido = Account.get_pedido!(id)
     changeset = Account.change_pedido(pedido)
 #    render(conn, "edit.html", pedido: pedido, changeset: changeset)
-     render(conn, "new.html", changeset: changeset, condpagamento: Repo.all(from(c in Condpag, select: {c.descricao_condpagamento, c.codigo_condpagamento})), cod_vendedor: Repo.all(from(c in Vendedor, select: {c.cod_vendedor, c.cod_vendedor})))
+     render(conn, "edit.html", pedido: pedido, changeset: changeset,
+     condpagamento: Repo.all(from(c in Condpag, select: {c.descricao_condpagamento, c.codigo_condpagamento})),
+     cod_vendedor: Repo.all(from(c in Vendedor, select: {c.cod_vendedor, c.cod_vendedor})))
   end
-
+################################################################################################################
   def update(conn, %{"id" => id, "pedido" => pedido_params}) do
     pedido = Account.get_pedido!(id)
 
@@ -70,10 +93,12 @@ defmodule SedpoolWeb.PedidoController do
         |> redirect(to: pedido_path(conn, :show, pedido))
       {:error, %Ecto.Changeset{} = changeset} ->
 #        render(conn, "edit.html", pedido: pedido, changeset: changeset)
-     render(conn, "new.html", changeset: changeset, condpagamento: Repo.all(from(c in Condpag, select: {c.descricao_condpagamento, c.codigo_condpagamento})), cod_vendedor: Repo.all(from(c in Vendedor, select: {c.cod_vendedor, c.cod_vendedor})))
+     render(conn, "edit.html", pedido: pedido, changeset: changeset,
+	condpagamento: Repo.all(from(c in Condpag, select: {c.descricao_condpagamento, c.codigo_condpagamento})),
+	cod_vendedor: Repo.all(from(c in Vendedor, select: {c.cod_vendedor, c.cod_vendedor})))
     end
   end
-
+###############################################################################################################
   def delete(conn, %{"id" => id}) do
     pedido = Account.get_pedido!(id)
     {:ok, _pedido} = Account.delete_pedido(pedido)
